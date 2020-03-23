c9faac9a4dfcbc318eb251ce4d1366ea7b9bd55a
Aditya Chandeliya <adityachandeliya@gmail.com>
2020-03-23 11:15:39 +0530

    chandeliya | adding product viewset and serializer

--- company/viewsets.py
+++ company/viewsets.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from . import serializers
 from . import models
 
@@ -9,3 +10,12 @@ class CompanyViewSet(viewsets.ModelViewSet):
     """
     queryset = models.CompanyModel.objects.all()
     serializer_class = serializers.CompanySerializer
+
+    # def create(self, request, *args, **kwargs):
+    #
+    #     CompanyViewSet.create()
+    #     super(CompanyViewSet, self).create(request, args, kwargs)
+    #     response = {"status_code": status.HTTP_200_OK,
+    #                 "message": "Successfully created",
+    #                 "result": request.data}
+    #     return Response(response)
